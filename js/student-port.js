bef64cab17102473f9fcac26394c0806a8641d94
julia-thummel <julia.thummel@wustl.edu>
2016-04-27 18:21:06 -0500

    j

---
+++ js/student-port.js
@@ -0,0 +1,14 @@
+var message = '';
+var student;
+
+function print(message) {
+  document.write("<section>" + message + "</section>")
+}
+
+for (var i = 0; i < students.length; i++) {
+  student = students[i];
+  message += '<h2>' + student.name + '</h2>';
+  message += '<p>' + student.track + '</p>';
+}
+
+print(message);
